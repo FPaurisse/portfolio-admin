f9378fc3b5d0c6a6e6078cebf97b5e291a0edb81
FPaurisse <paurissefrederic@gmail.com>
2020-03-01 12:30:49 +0100

    add delete route

--- src/pages/WorksListPage.js
+++ src/pages/WorksListPage.js
@@ -8,6 +8,10 @@ const WorksListPage = () => {
     api.get('/works').then((result) => setWorks(result.data));
   }, []);
 
+  const deleteWork = (id) => {
+    api.delete(`works/${id}`);
+  };
+
   return (
     <div className="WorksListPage">
       <Link to="/create">Create</Link>
@@ -15,7 +19,7 @@ const WorksListPage = () => {
         <div key={work.id}>
           <p>{work.title}</p>
           <Link to={`/edit/${work.slug}`}>Edit</Link>
-          <button type="button">Delete</button>
+          <button type="button" onClick={() => deleteWork(work.id)}>Delete</button>
         </div>
       ))}
     </div>
